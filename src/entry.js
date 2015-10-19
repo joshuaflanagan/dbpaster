b0306ec0ade82fd7c687935716202ab5e3d1db7b
Joshua Flanagan <joshuaflanagan@gmail.com>
2015-10-19 09:28:51 -0500

    Pull in React
    Based on https://underthehood.myob.com/changing-of-the-guard-in-web-technologies/

---
+++ src/entry.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+React.render(
+  <h1>My hotloading Example</h1>,
+  document.getElementById('content')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,58 @@
+'use strict';
+
+var webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  path = require('path'),
+  srcPath = path.join(__dirname, 'src');
+
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
+  target: 'web',
+  cache: true,
+  entry: {
+    module: path.join(srcPath, 'entry.js'),
+    common: ['react']
+  },
+  resolve: {
+    root: srcPath,
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules', 'src']
+  },
+  output: {
+    path: path.join(__dirname, 'tmp'),
+    publicPath: '',
+    filename: '[name].js',
+    library: ['DbPaster', '[name]'],
+    pathInfo: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+          'react-hot',
+          'babel?cacheDirectory&stage=0' // stage 0 allows experimental features
         ]
-    }
+      }
+      //, { test: /\.css$/, loader: "style!css" }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html'
+    }),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  debug: true,
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: './tmp',
+    historyApiFallback: true
+  }
 };
+
